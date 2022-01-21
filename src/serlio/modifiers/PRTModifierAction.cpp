bf64f08f9e2ac28f5628f2587de22ff0938b5bad
Christian Knieling <cknieling@esri.com>
2022-01-21 09:51:16 +0100

    Omit skipping empty string enum fields

--- src/serlio/modifiers/PRTModifierAction.cpp
+++ src/serlio/modifiers/PRTModifierAction.cpp
@@ -633,9 +633,6 @@ MStatus PRTModifierAction::updateDynamicEnums() {
 					const wchar_t *const *stringArray = mGenerateAttrs->getStringArray(valuesAttr, &arr_length);
 					
 					for (short enumIndex = 0; enumIndex < arr_length; enumIndex++) {
-						if (wcslen(stringArray[enumIndex]) == 0)
-							continue;
-
 						std::wstring currString = stringArray[enumIndex];
 
 						//remove newlines from strings, because they break the maya UI
