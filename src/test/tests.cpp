c9d3c22ec9444b999567f8316ba1caa624c4f643
Christian Knieling <cknieling@esri.com>
2022-03-30 09:13:37 +0200

    Add test for cleanForMaya

--- src/test/tests.cpp
+++ src/test/tests.cpp
@@ -428,6 +428,39 @@ TEST_CASE("replaceAllOf") {
 	}
 }
 
+TEST_CASE("cleanForMaya") {
+	SECTION("empty") {
+		std::wstring testString = L"";
+		const std::wstring expected = L"";
+		const std::wstring output = prtu::cleanNameForMaya(testString);
+		CHECK(output == expected);
+	}
+	SECTION("replace nothing") {
+		std::wstring testString = L"The_quick_brown_fox_jumps_over_the_lazy_dog";
+		const std::wstring expected = L"The_quick_brown_fox_jumps_over_the_lazy_dog";
+		const std::wstring output = prtu::cleanNameForMaya(testString);
+		CHECK(output == expected);
+	}
+	SECTION("replace some") {
+		std::wstring testString = L"Replace:all\r\nbut=alpha^numerical.characters;";
+		const std::wstring expected = L"Replace_all__but_alpha_numerical_characters_";
+		const std::wstring output = prtu::cleanNameForMaya(testString);
+		CHECK(output == expected);
+	}
+	SECTION("replace all") {
+		std::wstring testString = L"/:\r^?=-\\%`*\"+-";
+		const std::wstring expected = L"______________";
+		const std::wstring output = prtu::cleanNameForMaya(testString);
+		CHECK(output == expected);
+	}
+	SECTION("add prefix") {
+		std::wstring testString = L"42";
+		const std::wstring expected = L"_42";
+		const std::wstring output = prtu::cleanNameForMaya(testString);
+		CHECK(output == expected);
+	}
+}
+
 // we use a custom main function to manage PRT lifetime
 int main(int argc, char* argv[]) {
 	const std::vector<std::wstring> addExtDirs = {
